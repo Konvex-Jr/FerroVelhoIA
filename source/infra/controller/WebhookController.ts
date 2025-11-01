1ba910175cf27c2b8bc1c20d5161bdeec93d2e70
Lucas Spagnoli <lm.spanholi@gmail.com>
2025-11-01 16:31:31 -0300

    Arquivo pro webhook criado
    source->infra->controller

---
+++ source/infra/controller/WebhookController.ts
@@ -0,0 +1,14 @@
+// #TODO: webhook do whats com o seguinte curl:
+
+/*
+curl --request POST \
+  --url 'https://ferro-velho-evolution-api.ixbtpi.easypanel.host/instance/create?=' \
+  --header 'Content-Type: application/json' \
+  --header 'User-Agent: insomnia/11.2.0' \
+  --header 'apikey: 429683C4C977415CAAFCCE10F7D57E11' \
+  --data '{
+	"instanceName": "KONVEXJRTESTE",
+	"number": "5551983225533",
+	"integration": "WHATSAPP-BAILEYS"
+}'
+*/
